8fc5e4419c93b8100ee30ec30db825e777dee2da
Telepenin Nikolay <prefer@prefer-2.local>
2014-03-14 16:27:20 +0400

    Добавлен Google Analitics

--- _source/conf.py
+++ _source/conf.py
@@ -139,6 +139,7 @@ POSTS = (
     ("posts/*.rst", "posts", "post.tmpl"),
     ("posts/*.txt", "posts", "post.tmpl"),
 )
+
 PAGES = (
     ("stories/*.md", "stories", "story.tmpl"),
     ("stories/*.rst", "stories", "story.tmpl"),
@@ -611,7 +612,18 @@ SOCIAL_BUTTONS_CODE = ""
 
 # Extra things you want in the pages HEAD tag. This will be added right
 # before </head>
-# EXTRA_HEAD_DATA = ""
+EXTRA_HEAD_DATA = """
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-49005120-1', 'barsgroup.github.io');
+  ga('send', 'pageview');
+
+</script>
+"""
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 # BODY_END = ""
